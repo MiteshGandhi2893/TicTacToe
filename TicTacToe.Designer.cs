f0255f76c1801d1ab7d54ee9479b5e01dced6a04
MiteshGandhi2893 <miteshgandhi28@gmail.com>
2017-01-29 13:08:02 +0530

    Add files via upload

---
+++ TicTacToe.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace TICTACTO
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(468, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 60);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(579, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 60);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(687, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 60);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(468, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 60);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(579, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 60);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(687, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 60);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button7.Location = new System.Drawing.Point(468, 260);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 60);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button8.Location = new System.Drawing.Point(579, 260);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 60);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.button9.Location = new System.Drawing.Point(687, 260);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 60);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "player1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "player2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "X",
+            "0"});
+            this.comboBox1.Location = new System.Drawing.Point(273, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(46, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "X",
+            "0"});
+            this.comboBox2.Location = new System.Drawing.Point(273, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(46, 21);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(348, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(348, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 17;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(16, 149);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(121, 23);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "play";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(84, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(84, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 295);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 341);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(194, 20);
+            this.textBox4.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(190, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 24);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "SCORE";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(164, 149);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 23);
+            this.button11.TabIndex = 24;
+            this.button11.Text = "play again";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(295, 149);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(97, 23);
+            this.button12.TabIndex = 25;
+            this.button12.Text = "quit";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 461);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ TicTacToe.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TICTACTO
+{
+    public partial class TicTacToe : Form
+    {
+        int score1 = 0,score2=0;
+        string sname="";
+        string p1 = "";
+        string p2 = "";
+        public TicTacToe()
+        {
+            InitializeComponent();
+            buttarray=new Button[9]{button1,button2,button3,button4,button5,button6,button7,button8,button9};
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool _isGameOver=false, _isX=true;
+        string symbol="";
+        int a = 0;
+        int b = 0;
+        private void ClickHandler(object sender, System.EventArgs e)
+        {
+            
+            Button tempButton = (Button)sender;
+
+            if (this._isGameOver)
+            {
+                MessageBox.Show("Game Over...Select Play for a new game!",
+                                "ERROR", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (tempButton.Text != "")    // if not empty..already has X or O
+            {
+                //MessageBox.Show("Button already has value!", "ERROR", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (_isX)    // Alternate putting the X or O character in the Text property
+                tempButton.Text = label2.Text;
+            else
+                tempButton.Text = label4.Text;
+
+            _isX = !_isX;    // 
+            
+          
+         TicTacToe.CheckAndProcessWinner(buttarray,out this._isGameOver,out symbol);
+       //  MessageBox.Show(symbol+"    "+a+"    "+b);
+         if (_isGameOver)
+         {
+           
+             sname = symbol;
+             
+          
+             
+     
+             if (sname == label2.Text)
+             {
+               
+                 textBox3.Text = "" + (Convert.ToInt16(textBox3.Text)+1);
+                 MessageBox.Show(textBox1.Text + "  " + "wins");
+             }
+             else
+             {
+
+                 textBox4.Text = "" + (Convert.ToInt16(textBox4.Text) + 1);
+                 MessageBox.Show(textBox2.Text + "  " + "wins");
+             }
+         }
+         _isGameOver = false;
+       //  _isX = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = comboBox1.SelectedItem.ToString();
+            if (label2.Text == "X")
+            {
+                label4.Text = "0";
+                comboBox2.Text = label4.Text;
+            }
+            else
+            {
+                label4.Text = "X";
+                comboBox2.Text = label4.Text;
+            }
+            p1 = label2.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.Text = comboBox2.SelectedItem.ToString();
+            if (label4.Text == "X")
+            {
+                label2.Text = "0";
+                comboBox1.Text = label2.Text;
+
+            }
+            else
+            {
+                label2.Text = "X";
+                        comboBox1.Text = label2.Text;
+            }
+            p2 = label4.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "" && textBox2.Text == "") || (textBox1.Text == "" || textBox2.Text == ""))
+            {
+                MessageBox.Show("plz fill all the textbox");
+            }
+            else if (comboBox1.SelectedIndex < 0 || comboBox2.SelectedIndex < 0)
+            {
+                MessageBox.Show("plz select your letter");
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+               comboBox1.Enabled = false;
+               textBox3.Text = "0";
+               textBox4.Text = "0";
+           comboBox2.Enabled = false;
+                label5.Text = textBox1.Text;
+                label6.Text = textBox2.Text;
+                for (int i = 0; i < 9; i++)
+                {
+                    this.buttarray[i].Click += new System.EventHandler(this.ClickHandler);
+                   
+                }
+               
+            }
+            button10.Visible = false;
+
+        }
+        static public void CheckAndProcessWinner(Button[] myControls,out bool gameOver,out string symbol)
+        {
+           
+            gameOver = false;
+            symbol = "";
+           
+            for (int i = 0; i < 8; i++)
+            {
+                int a = Winners[i, 0], b = Winners[i, 1], c = Winners[i, 2];
+                Button b1 = myControls[a], b2 = myControls[b], b3 = myControls[c];
+
+                if (b1.Text == "" || b2.Text == "" || b3.Text == "") // if one if blank
+                    continue;    // try another -- no need to go further
+
+                if (b1.Text == b2.Text && b2.Text == b3.Text)
+                {
+                    b1.BackColor = b2.BackColor = b3.BackColor = Color.LightCoral;
+                    b1.Font = b2.Font = b3.Font
+                         = new System.Drawing.Font("Microsoft Sans Serif", 32F,
+                                                   System.Drawing.FontStyle.Italic &
+                                                   System.Drawing.FontStyle.Bold,
+                                                   System.Drawing.GraphicsUnit.Point,
+                                                   ((System.Byte)(0)));
+                     TicTacToe f1=new TicTacToe();
+                   
+                    symbol = b1.Text;
+                    gameOver = true;
+                   
+                    break;
+                    
+                }
+            }
+           
+        }
+        static private int[,] Winners = new int[,]
+   {
+    {0,1,2},
+    {3,4,5},
+    {6,7,8},
+    {0,3,6},
+    {1,4,7},
+    {2,5,8},
+    {0,4,8},
+    {2,4,6}
+   };
+        Button[] buttarray;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+           
+           
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < 9; i++)
+            {
+                buttarray[i].Text = "";
+
+                buttarray[i].BackColor = Color.White;
+                buttarray[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 32F,
+                                                   System.Drawing.FontStyle.Italic &
+                                                   System.Drawing.FontStyle.Bold,
+                                                   System.Drawing.GraphicsUnit.Point,
+                                                   ((System.Byte)(0)));
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                this.buttarray[i].Click += new System.EventHandler(this.ClickHandler);
+
+            }
+        }
+
+      
+    }
+}
